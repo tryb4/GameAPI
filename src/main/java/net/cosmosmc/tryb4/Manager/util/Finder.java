1b42201e6fd4ebafdbdeb47d104441ed2d43e4e2
tryb4 <danieel.boman@yahoo.com>
2014-01-31 20:25:44 +0100

    Changes... duh

--- src/main/java/net/cosmosmc/tryb4/Manager/util/Finder.java
+++ src/main/java/net/cosmosmc/tryb4/Manager/util/Finder.java
@@ -1,6 +1,6 @@
 package net.cosmosmc.tryb4.Manager.util;
 
-import net.cosmosmc.tryb4.Manager.Manager;
+import net.cosmosmc.tryb4.Manager.GameAPI;
 import org.bukkit.*;
 import org.bukkit.entity.*;
 import org.bukkit.inventory.*;
@@ -20,12 +20,12 @@ public class Finder
     public static ItemStack getCompass(Player pl) throws Exception{
         Player closest = null;
         int close = Integer.MAX_VALUE / 2;
-        for (Player p : Manager.getGame(pl).getPlayers()) {
+        for (Player p : GameAPI.getGame(pl).getPlayers()) {
             if ((int)p.getLocation().distance(pl.getLocation()) < close) {
                 close = (int) p.getLocation().distance(pl.getLocation());
             }
         }
-        for (Player p : Manager.getGame(pl).getPlayers()) {
+        for (Player p : GameAPI.getGame(pl).getPlayers()) {
             if (p.getLocation().distance(pl.getLocation()) <= close) {
                 closest = p;
             }
